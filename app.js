d2a7fa187f3c911d3efac7d20d814045f5d71fb8
ibrahim-sert <evo3506@gmail.com>
2023-01-22 22:36:47 +0300

    Added all

--- app.js
+++ app.js
@@ -9,23 +9,29 @@ let sum = 0;
 
 sec.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn-add")) {
-    sum++;
-    const li = document.createElement("li");
-    li.setAttribute("class", "class");
-    if (sum > 2) {
-      const div = document.querySelector(".iframe");
-      div.innerHTML = `<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="480" src="https://giphy.com/embed/r8NV9HQpuhDcLDAP2I/video" width="480"></iframe></div>`;
+    if (input.value === "") {
+      alert("Lütfen bir kaç iş kös kocana");
+    } else {
+      sum++;
+      const li = document.createElement("li");
+      li.setAttribute("class", "class");
+      if (sum > 2) {
+        const div = document.querySelector(".iframe");
+        div.innerHTML = `<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="480" src="https://giphy.com/embed/r8NV9HQpuhDcLDAP2I/video" width="480"></iframe></div>`;
+      }
+      const icon = document.createElement("i");
+      icon.setAttribute("class", "fas fa-check");
+      li.append(icon);
+      const p = document.createElement("p");
+      p.innerText = input.value;
+      li.appendChild(p);
+      const removeIcon = document.createElement("i");
+      removeIcon.setAttribute("class", "fas fa-trash");
+      li.append(removeIcon);
+      ul.prepend(li);
+      input.focus();
+      input.value = "";
     }
-    const icon = document.createElement("i");
-    icon.setAttribute("class", "fas fa-check");
-    li.append(icon);
-    const p = document.createElement("p");
-    p.innerText = input.value;
-    li.appendChild(p);
-    const removeIcon = document.createElement("i");
-    removeIcon.setAttribute("class", "fas fa-trash");
-    li.append(removeIcon);
-    ul.prepend(li);
   }
 });
 
@@ -34,5 +40,11 @@ butonCtn.addEventListener("click", (e) => {
     e.target.closest(".class").remove();
   } else if (e.target.classList.contains("fa-check")) {
     e.target.closest(".class").classList.toggle("checked");
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    button.click();
   }
 });
